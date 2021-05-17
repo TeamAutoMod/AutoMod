5db89ecf459db659c363b10825ced00050c3fd12
xezzz <pschaper18@gmail.com>
2021-05-17 21:38:52 +0200

    Fix names

---
+++ harpoon/src/migrations/Migration.py
@@ -0,0 +1,11 @@
+
+
+
+
+class Migrations:
+    def __init__(self, db):
+        self.db = db
+
+
+    def run(self):
+        pass
---
+++ update.bat
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+git pull origin master
+cd ./automod
+py -3 ./start.py
