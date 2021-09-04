b75fc2ece03d1aae8fed80a6135513c71ec77f77
xezzz <pschaper18@gmail.com>
2021-09-04 23:51:32 +0200

    More starboard fixes

--- automod/src/plugins/Starboard/events/OnRawReactionAdd.py
+++ automod/src/plugins/Starboard/events/OnRawReactionAdd.py
@@ -14,6 +14,9 @@ async def run(plugin, payload: discord.RawReactionActionEvent):
     if guild is None:
         return
 
+    if str(payload.user_id) == str(plugin.bot.user.id):
+        return
+
     config = plugin.db.configs.get(f"{payload.guild_id}", "starboard")
     if config["enabled"] == False or config["channel"] == "":
         return
@@ -21,12 +24,15 @@ async def run(plugin, payload: discord.RawReactionActionEvent):
     if payload.channel_id in config["ignored_channels"]:
         return
 
-    starbord_channel = await plugin.bot.utils.getChannel(guild, int(config["channel"]))
-    if starbord_channel == None:
+    starboard_channel = await plugin.bot.utils.getChannel(guild, int(config["channel"]))
+    if starboard_channel == None:
         return
 
     current_channel = await plugin.bot.utils.getChannel(guild, payload.channel_id)
     if current_channel == None:
+        return
+
+    if str(starboard_channel.id) == str(current_channel.id):
         return
 
     message = await current_channel.fetch_message(payload.message_id)
@@ -36,4 +42,4 @@ async def run(plugin, payload: discord.RawReactionActionEvent):
     stars, new = get_stars_for_message(plugin, message)
     embed, content = build_embed(message, stars)
 
-    await edit_or_send(plugin, new, message, starbord_channel, content, embed)
+    await edit_or_send(plugin, new, message, starboard_channel, content, embed)
