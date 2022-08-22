12f60411dabef76df69d9fce2ecc70353be15704
xezzz <pschaper18@gmail.com>
2022-08-22 20:15:02 +0200

    Fix for version check

--- launch.py
+++ launch.py
@@ -11,10 +11,7 @@ from packages.bot import ShardedBotInstance
 
 
 if __name__ == "__main__":
-    if discord.__version__ != "2.0.0a":
+    if not inspect.iscoroutinefunction(commands.Bot.load_extension):
         print("You're discord.py version is too old. Install a newer one using the following command: \npip install git+https://github.com/Rapptz/discord.py"); sys.exit(1)
     else:
-        if not inspect.iscoroutinefunction(commands.Bot.load_extension):
-            print("You're discord.py version is too old. Install a newer one using the following command: \npip install git+https://github.com/Rapptz/discord.py"); sys.exit(1)
-        else:
-           ShardedBotInstance()
+        ShardedBotInstance()
