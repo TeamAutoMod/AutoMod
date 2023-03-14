cd5591c0909cd7799f105f2b303be94cd5397b4b
xezzz <pschaper18@gmail.com>
2023-03-14 17:31:30 +0100

    Fix

--- automod/plugins/level/plugin.py
+++ automod/plugins/level/plugin.py
@@ -112,10 +112,10 @@ class LevelPlugin(AutoModPluginBlueprint):
     
 
     def add_missing_attrs(self, guild: discord.Guild, doc: dict) -> None:
-        if "rewards" not in:
+        if "rewards" not in doc:
             doc["rewards"] = {}
         else:
-            if doc["rewards"] = None:
+            if doc["rewards"] == None:
                 doc["rewards"] = {}
         if "reward_mode" not in doc:
             doc["reward_mode"] = "stack"
